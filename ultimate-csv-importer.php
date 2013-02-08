c2e77593217754d9e3d9468eb5c87c3a3df486a1
Brian DiChiara <briandichiara@gmail.com>
2013-02-08 14:56:38 -0600

    re-re-update to 2.7

--- ultimate-csv-importer.php
+++ ultimate-csv-importer.php
@@ -1,25 +1,1 @@
-<<<<<<< HEAD
-<?php
-/*
-Plugin Name: BMoney Ultimate CSV Importer
-Plugin URI: https://github.com/solepixel/bmoney-ultimate-csv-importer/
-Description: Imports (almost) anything into WordPress.
-Version: 2.6
-Author: Brian DiChiara
-Author URI: http://www.briandichiara.com
-*/
-
-define('BMUCI_VERSION', '2.6');
-define('BMUCI_PI_NAME', 'Ultimate CSV Importer');
-define('BMUCI_PI_DESCRIPTION', 'Imports (almost) anything into WordPress.');
-define('BMUCI_OPT_PREFIX', 'bmuci_');
-define('BMUCI_PATH', plugin_dir_path( __FILE__ ));
-define('BMUCI_DIR', plugin_dir_url( __FILE__ ));
-
-require_once(BMUCI_PATH.'classes/bm-ultimate-csv-importer.class.php');
-
-$bmuci_plugin = new BM_Ultimate_CSV_Importer();
-$bmuci_plugin->initialize();
-=======
-<?php/** * Plugin Name: BMoney Ultimate CSV Importer * Plugin URI: https://github.com/solepixel/bmoney-ultimate-csv-importer/ * Description: Imports (almost) anything into WordPress. * Version: 2.61 * Author: Brian DiChiara * Author URI: http://www.briandichiara.com */define('BMUCI_VERSION', '2.61');define('BMUCI_PI_NAME', 'Ultimate CSV Importer');define('BMUCI_PI_DESCRIPTION', 'Imports (almost) anything into WordPress.');define('BMUCI_OPT_PREFIX', 'bmuci_');define('BMUCI_PATH', plugin_dir_path( __FILE__ ));define('BMUCI_DIR', plugin_dir_url( __FILE__ ));require_once(BMUCI_PATH.'classes/bm-ultimate-csv-importer.class.php');global $bmuci_plugin;$bmuci_plugin = new BM_Ultimate_CSV_Importer();$bmuci_plugin->initialize();
->>>>>>> 51ba784e3cef9bcb0ef6f3292d1d86c6e935fbe8
+<?php/** * Plugin Name: BMoney Ultimate CSV Importer * Plugin URI: https://github.com/solepixel/bmoney-ultimate-csv-importer/ * Description: Imports (almost) anything into WordPress. * Version: 2.7 * Author: Brian DiChiara * Author URI: http://www.briandichiara.com */define('BMUCI_VERSION', '2.7');define('BMUCI_PI_NAME', 'Ultimate CSV Importer');define('BMUCI_PI_DESCRIPTION', 'Imports (almost) anything into WordPress.');define('BMUCI_OPT_PREFIX', 'bmuci_');define('BMUCI_PATH', plugin_dir_path( __FILE__ ));define('BMUCI_DIR', plugin_dir_url( __FILE__ ));include_once(BMUCI_PATH.'updater/updater.php');require_once(BMUCI_PATH.'classes/bm-ultimate-csv-importer.class.php');global $bmuci_plugin;$bmuci_plugin = new BM_Ultimate_CSV_Importer();$bmuci_plugin->initialize();
